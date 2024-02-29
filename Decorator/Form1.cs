ca21fea534ca30414b39d14ac7c96ee46cf6450c
kyotakata <57854222+kyotakata@users.noreply.github.com>
2024-03-01 07:24:10 +0900

    プロジェクト ファイルを追加します。

---
+++ Decorator/Form1.cs
@@ -0,0 +1,19 @@
+using Decorator.Objects;
+
+namespace Decorator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var conponent = new ComponentA();
+            var value = conponent.GetData();
+            button1.Text = value;
+        }
+    }
+}
